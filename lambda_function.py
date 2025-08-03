01f6ada7608f7f04adebc2c90da03193e4369a32
Arthur Coutinho <arthurtelescoutinho@gmail.com>
2025-08-03 19:35:36 -0300

    feat: adicionando variável de ambiente no log

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,11 @@
 import json
 from log import log
+import os
 
 def lambda_handler(event, context):
     # TODO implement
     log(f'Log de execução após GitHub Actions. {json.dumps(event)}')
+    
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
--- log.py
+++ log.py
@@ -1,2 +1,5 @@
+import os 
+
 def log(mensagem):
-    return print('Adicionando log via função', mensagem)
+    print(f'Mostrando variável de ambiente: {os.getenv('MINHA_VAR')}')
+    print('Adicionando log via função', mensagem)
