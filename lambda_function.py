805c803414642fa40ba39d1b1e31bee05162f5e9
Arthur Coutinho <arthurtelescoutinho@gmail.com>
2025-08-03 15:01:32 -0300

    feat: alterando log

--- lambda_function.py
+++ lambda_function.py
@@ -3,7 +3,7 @@ from log import log
 
 def lambda_handler(event, context):
     # TODO implement
-    log(f'Log de execução {json.dumps(event)}')
+    log(f'Log de execução após GitHub Actions. {json.dumps(event)}')
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
