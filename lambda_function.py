4834f2409b26cafdb0d7c616aa125afe869d9cef
Arthur Coutinho <arthurtelescoutinho@gmail.com>
2025-08-03 20:20:33 -0300

    feat: adicionando resposta a leitura em s3

--- lambda_function.py
+++ lambda_function.py
@@ -1,17 +1,27 @@
 import json
 from log import log
+
 import os
+import boto3
+
+boto3.setup_default_session(region_name='us-east-1')
+s3 =  boto3.client('s3')
 
 def lambda_handler(event, context):
-    # TODO implement
-    log(f'Log de execução após GitHub Actions. {json.dumps(event)}')
+    record = event['Records'][0]
+    bucket = record['s3']['bucket']['name']
+    key = record['s3']['bucket']['key']
     
-    return {
-        'statusCode': 200,
-        'body': f'<html><body>Dados da requisição: {json.dumps(event)} </body></html>',
-        'headers': {
-            'content-type': 'text/html'
-        } 
-    }
+    response = s3.head_object(Bucket=bucket, Key=key)  
+    content_length = response['ContentLength']   
+    
+    mega_byte = 1024 * 1024
+    
+    if content_length > mega_byte:
+        log('Objeto muito grande')
+        return 'Objeto muito grande'
+    
+    
+    return log('Objeto de tamanho OK')
 
 
